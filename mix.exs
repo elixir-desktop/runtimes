c5256c692f9591fa938e90941fc52535ad955489
adiibanez <adi.ibanez@freestyleair.com>
2025-02-24 13:25:53 +0100

    Trigger ci

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Runtimes.MixProject do
   def project do
     [
       app: :runtimes,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
